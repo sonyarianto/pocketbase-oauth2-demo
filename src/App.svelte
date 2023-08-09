594c74520121418eee2f819a41cf3f58f152bb99
sonyarianto <sony@sony-ak.com>
2023-08-09 09:43:49 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import PocketBase from 'pocketbase';
+
+  async function signIn() {
+    const pb = new PocketBase('https://api.buka.sh/v2');
+
+    const authData = await pb.collection('users').authWithOAuth2({ provider: 'google' });
+
+    // after the above you can also access the auth data from the authStore
+    
+    console.log(pb.authStore.isValid);
+    console.log(pb.authStore.token);
+    console.log(pb.authStore.model.id);
+  }
+</script>
+
+<button on:click={signIn}>Sign-in with Google</button>
+
